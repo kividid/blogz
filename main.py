6db781c60ee73dcc2e9b58d50b61d1632023cbdc
Sam Thompson <sam@kividid.com>
2018-10-31 19:52:40 -0500

    initial main.py setup

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:Or.r0cks@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+
+db = SQLAlchemy(app)
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    date = db.Column(db.DateTime)
+    content = db.Column(db.String(1000))
