4be9f47032a4e71d9d4022b111de4588d097f4f3
Sam Thompson <sam@kividid.com>
2018-11-01 19:38:42 -0500

    started creating templates, listing blog posts

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -8,8 +9,40 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
 
+
+def _get_date():
+    return datetime.datetime.now()
+
+
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60))
-    date = db.Column(db.DateTime)
+    post_date = db.Column(db.DateTime, default=_get_date)
     content = db.Column(db.String(1000))
+
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
+
+@app.route('/blog')
+def blog():
+    blogs = Blog.query.all()
+
+    return render_template('blog.html', title='Blog', blogs=blogs)
+
+
+@app.route('/newpost', methods=['GET', 'POST'])
+def newpost():
+
+    if request.method == 'POST':
+        
+        return redirect('blog.html')
+
+    return render_template('newpost.html', title='Create New Post')
+
+
+app.run()
